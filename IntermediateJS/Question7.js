2605bcfef902485894b3a55e42ae3d36e1c66314
popo26 <mynameispopo26@gmail.com>
2023-08-27 07:23:03 +1200

    IntermediateJS-Question7 completed

--- IntermediateJS/Question7.js
+++ IntermediateJS/Question7.js
@@ -15,6 +15,7 @@ const books = [
 function getBookTitle(bookId) {
     return books.find(book => book.id === bookId);
 };
+console.log("\r\n==============function getBookTitle(bookId)==============")
 console.log(getBookTitle(1));
 
 // b) Write a function getOldBooks() that uses the filter function to return all book objects written before 1950.
@@ -22,6 +23,7 @@ console.log(getBookTitle(1));
 function getOldBooks() {
     return books.filter(book => book.year < 1950);
 };
+console.log("\r\n==============function getOldBooks()==============")
 console.log(getOldBooks());
 
 // c) Write a function addGenre() that uses the map function to add a new genre property to all of the above books, with the value ‘classic’. 
@@ -29,20 +31,34 @@ console.log(getOldBooks());
 function addGenre() {
     return books.map(book => ({ ...book, genre: "classic" }));
 };
-
+console.log("\r\n==============function addGenre()==============")
 console.log(addGenre());
 
 // d) (Extension) Write a function getTitles(authorInitial) that uses map and filter together to return an array of book titles for books written by authors whose names start with authorInitial. 
 
 function getTitles(authorInitial) {
-    return books.map(book => book.title)
-        .filter(book => book.author.startsWith(authorInitial));
+    return books.filter((book) => book.author.toLowerCase().startsWith(authorInitial.toLowerCase())).map(book => book.title);
 };
-
-console.log(getTitles("F"));
+console.log("\r\n==============function getTitles(authorInitial)==============")
+console.log(getTitles("h"));
 
 // e) (Extension) Write a function latestBook() that uses find and forEach to get the book with the most recent publication date.
 
 function latestBook() {
-    return;
-}
+    let mostRecentYear = 0;
+    const result = books.forEach((book) => {
+        if (book.year > mostRecentYear) {
+            mostRecentYear = book.year;
+        };
+    });
+    return books.find(book => book.year === mostRecentYear)
+};
+
+function latestBook2() {
+    let mostRecentYear = 0;
+    books.forEach(book => book.year > mostRecentYear ? mostRecentYear = book.year : null);
+    return books.find(book => book.year === mostRecentYear)
+};
+console.log("\r\n==============function latestBook()==============")
+console.log(latestBook());
+console.log(latestBook2());
